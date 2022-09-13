c6abdcacc840b27db080e8520b84224057a64219
Favour919 <favourbassey999@gmail.com>
2022-09-13 07:46:27 -0700

    task1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,8 +3,12 @@
  * main - print_alphabet
  * Return: 0
  */
-int main(void)
+void print_alphabet(void)
 {
-print_alphabet();
-return (0);
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
 }
