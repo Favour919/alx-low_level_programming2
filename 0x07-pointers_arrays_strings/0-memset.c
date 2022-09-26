b9be01bda12b74e7097966fe9ba71bca600b0be9
Favour919 <favourbassey999@gmail.com>
2022-09-26 04:32:30 -0700

    task0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _memset - fills memory with a constant byte
  * @s: pointer block of memory to fill
@@ -6,6 +7,7 @@
  * @n: bytes of the memory
  * Return: dest
  */
+
 
 char *_memset(char *s, char b, unsigned int n)
 {
