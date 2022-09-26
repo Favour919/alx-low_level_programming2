ba2633ec5b97076ad8a16ce68c458e1d511c4516
Favour Bassey <67082766+Favour919@users.noreply.github.com>
2022-09-26 13:42:24 +0100

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -7,15 +7,14 @@
  * @n: max bytes to use
  * Return: s
  */
-
 char *_memset(char *s, char b, unsigned int n)
 {
-unsigned int i = 0;
+	unsigned int i = 0;
 
-while (i < n)
-{
-	*(s + i) = b;
-	i++;
-}
-return (s);
+	while (i < n)
+	{
+		*(s + i) = b;
+		i++;
+	}
+	return (s);
 }
