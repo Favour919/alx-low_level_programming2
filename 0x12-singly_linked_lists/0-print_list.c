1748ee9819c21cc569dc7084f068bd03d7d31b60
Favour Bassey <67082766+Favour919@users.noreply.github.com>
2022-10-21 12:53:10 +0100

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -17,8 +17,8 @@ size_t print_list(const list_t *h)
 			printf("[%d] %s\n", 0, "(nil)");
 		else
 			printf("[%d] %s\n", h->len, h->str);
-			h = h->next;
-			l_node++;
+		h = h->next;
+		l_node++;
 	}
 	return (l_node);
 }
