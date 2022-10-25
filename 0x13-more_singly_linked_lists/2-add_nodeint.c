8c1874c2d262cef9d752dce5abad3cf927467d06
Favour Bassey <67082766+Favour919@users.noreply.github.com>
2022-10-25 10:38:10 +0100

    Update 2-add_nodeint.c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,23 +1,40 @@
 #include "lists.h"
 
 /**
- * add_nodeint - adds a new node at the beginning of a linked list
- * @head: pointer to the first node in the list
- * @n: data to insert in that new node
+ * delete_nodeint_at_index - Deletes the node at a given
+ *                           index of a listint_t list.
+ * @head: A pointer to the address of the
+ *        head of the listint_t list.
+ * @index: The index of the node to be deleted - indices start at 0.
  *
- * Return: pointer to the new node, or NULL if it fails
+ * Return: On success - 1.
+ *         On failure - -1.
  */
-listint_t *add_nodeint(listint_t **head, const int n)
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-    listint_t *new;
+	listint_t *tmp, *copy = *head;
+	unsigned int node;
 
-    new = malloc(sizeof(listint_t));
-    if (!new)
-        return (NULL);
+	if (copy == NULL)
+		return (-1);
 
-    new->n = n;
-    new->next = *head;
-    *head = new;
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(copy);
+		return (1);
+	}
 
-    return (new);
+	for (node = 0; node < (index - 1); node++)
+	{
+		if (copy->next == NULL)
+			return (-1);
+
+		copy = copy->next;
+	}
+
+	tmp = copy->next;
+	copy->next = tmp->next;
+	free(tmp);
+	return (1);
 }
